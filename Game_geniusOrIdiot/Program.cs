7328a4e08bb30a6cd0250ab45c43c4dc954e267b
Rebel <agundadzidcoeva6530@gmail.com>
2025-11-26 20:04:15 +0300

    с помощью дипсика сделал перемешку,использовал словарь все остальное сделал сам в данную секунду иду разбирать как дипсик перемешал словарь

--- Game_geniusOrIdiot/Program.cs
+++ Game_geniusOrIdiot/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System.Xml;
+using System.Xml.Serialization;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Game_geniusOrIdiot
 {
@@ -6,45 +8,90 @@ namespace Game_geniusOrIdiot
     {
         static void Main(string[] args)
         {
-            string[] bankOfQuestions = { "Сколько океанов на планете Земля?", "Одно яйцо варится 3 минуты,сколько минут варятся три яйца?", "Сколько будет два плюс два умножить на два?", "Укол делают каждые полчаса.Сколько минут,сделать три укола?","Бревно нужно распилить на 10 частей.Сколько распилов нужно сделать?"};
-            string[] correctAnswers = { "4", "3", "6", "60","9"};
-            int cnt = 0;
-            List<string> mark = new List<string>();
-            Console.WriteLine("Добро пожаловать в игру Гений-Идиот");
-            Console.WriteLine();
-            for (int i = 0; i < bankOfQuestions.Length; i++)
+            string[] bankOfQuestions = { "Сколько океанов на планете Земля?", "Одно яйцо варится 3 минуты,сколько минут варятся три яйца?", "Сколько будет два плюс два умножить на два?", "Укол делают каждые полчаса.Сколько минут,сделать три укола?", "Бревно нужно распилить на 10 частей.Сколько распилов нужно сделать?" };
+            string[] correctAnswers = { "4", "3", "6", "60", "9" };
+
+            Dictionary<string, string> queANs = GiveMeDictionary(bankOfQuestions, correctAnswers);
+
+            Greeting();
+            string nameOfUser = Console.ReadLine();
+            Random rng = new Random();
+
+            while (true)
             {
-                Console.WriteLine(bankOfQuestions[i]);
-                string answer = Console.ReadLine();
-                if (answer == correctAnswers[i])
+                // Перемешиваем вопросы прямо здесь
+                var shuffledQuestions = queANs
+                    .OrderBy(x => rng.Next())
+                    .ToDictionary(pair => pair.Key, pair => pair.Value);
+
+                int cnt = 0;
+                List<string> mark = new List<string>();
+
+                foreach (string s in shuffledQuestions.Keys)
                 {
-                    cnt++;
-                    mark.Add("+");
+                    Console.WriteLine(s);
+                    string answer = Console.ReadLine();
+                    if (answer == shuffledQuestions[s])
+                    {
+                        cnt++;
+                        mark.Add("+");
+                    }
+                    else
+                    {
+                        mark.Add("-");
+                    }
+                }
+
+                Result(cnt, mark, correctAnswers);
+                Console.WriteLine($"Ваш диагноз:{nameOfUser}-{Diagnosis(cnt)}");
+                Console.WriteLine();
+                Console.WriteLine("Хотите сыграть заново?");
+                string choice = Console.ReadLine();
+                if (choice.ToLower() == "да")
+                {
+                    continue;
                 }
                 else
                 {
-                    mark.Add("-");
+                    break;
                 }
             }
-            Console.WriteLine($"Ваше количество правильных ответов : {cnt}");
-            Console.WriteLine("Результаты:");
-            for (int i = 0; i < correctAnswers.Length; i++)
-            {
-                Console.WriteLine($"{i + 1}){mark[i]}");
-            }
-            Console.WriteLine($"Ваш диагноз:{Diagnosis(cnt)}");
-            
-        }
-        static string Diagnosis(int cnt)
-        {
-            string[] diagnosises = {"Идиот", "Бездарь", "Человек Разумный", "Талант", "Гений" };
-            if (cnt == 0)
-            {
-                return diagnosises[0];
-            }
-            return diagnosises[cnt-1];
 
+            Console.WriteLine();
+            Console.WriteLine("Игра завершена. Удачи в следующий раз");
+
+            static string Diagnosis(int cnt)
+            {
+                string[] diagnosises = { "Идиот", "Бездарь", "Дурак", "Человек Разумный", "Талант", "Гений" };
+                return diagnosises[cnt];
+            }
+
+            static void Result(int cnt, List<string> mark, string[] correctAnswers)
+            {
+                Console.WriteLine($"Ваше количество правильных ответов : {cnt}");
+                Console.WriteLine("Результаты:");
+                for (int i = 0; i < mark.Count; i++)
+                {
+                    Console.WriteLine($"{i + 1}){mark[i]}");
+                }
+            }
+
+            static void Greeting()
+            {
+                Console.WriteLine("Добро пожаловать в игру Гений-Идиот");
+                Console.WriteLine();
+                Console.WriteLine("Введите ваше имя:");
+            }
+
+            static Dictionary<string, string> GiveMeDictionary(string[] bankOfQuestions, string[] correctAnswers)
+            {
+                Dictionary<string, string> queAns = new Dictionary<string, string>();
+                for (int i = 0; i < bankOfQuestions.Length; i++)
+                {
+                    queAns[bankOfQuestions[i]] = correctAnswers[i];
+                }
+                return queAns;
+            }
         }
-        //
     }
 }
