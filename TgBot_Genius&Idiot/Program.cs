9693316daf904fd925fe660475f8c018f0d94794
Rebel <agundadzidcoeva6530@gmail.com>
2026-03-11 20:53:13 +0300

    суета братя

--- TgBot_Genius&Idiot/Program.cs
+++ TgBot_Genius&Idiot/Program.cs
@@ -7,22 +7,20 @@ namespace TgBot_Genius_Idiot
     {
         static TelegramBotClient bot = new TelegramBotClient("8709825825:AAF5GH_GzfchJZKaSlngaC4b-PVNe-He8U0");
 
+        private static List<Question> questions;
         static int randomInd;
+        static int chatId = 1025006977;
+        static int correctAnswersCount;
+        static int questionCount;
         static async Task Main(string[] args)
         {
             var bot = new TelegramBotClient("8709825825:AAF5GH_GzfchJZKaSlngaC4b-PVNe-He8U0");
             var me = await bot.GetMe();
             Console.WriteLine($"Bot name is {me.FirstName}.");
-
-
-            Question question1 = new Question("скики", "мниги");
-            Question question2 = new Question("Ты мужик?", "да");
-            Question question3 = new Question("2=2*2?", "6");
-
-            List<Question> questions = new List<Question>();
-            questions.Add(question1);
-            questions.Add(question2);
-            questions.Add(question3);
+            
+            QuestionsStorage questionsStorage = new QuestionsStorage();
+            questions = questionsStorage.GetAll();
+            questionCount = questions.Count;
 
             bot.OnUpdate += Bot_OnUpdate;
 
@@ -31,37 +29,66 @@ namespace TgBot_Genius_Idiot
 
         private static async Task Bot_OnUpdate(Telegram.Bot.Types.Update update)
         {
-            Question question1 = new Question("скики", "мниги");
-            Question question2 = new Question("Ты мужик?", "да");
-            Question question3 = new Question("2=2*2?", "6");
 
-            List<Question> questions = new List<Question>();
-            questions.Add(question1);
-            questions.Add(question2);
-            questions.Add(question3);
-
-            Random rnd = new Random();
-            int randomInd = rnd.Next(0, questions.Count);
+            if (update.Message == null)
+                return;
 
             if (update.Message.Text == "/start")
             {
-                randomInd = new Random().Next(0, questions.Count);
-
-                await bot.SendMessage(update.Message.Chat.Id, questions[randomInd].Text);
-            }
-            else
-            {
-                if (update.Message.Text == questions[randomInd].RightAnswer)
+                if ((int)update.Message.Chat.Id == chatId)
                 {
-                    await bot.SendMessage(update.Message.Chat.Id, update.Message.Text = "Базару нет");
+                    randomInd = new Random().Next(0, questions.Count);
+
+                    await bot.SendMessage(update.Message.Chat.Id, questions[randomInd].Text);
+
                 }
                 else
                 {
-                    await bot.SendMessage(update.Message.Chat.Id, update.Message.Text = "ну неправильно бр");
+                    randomInd = new Random().Next(0, questions.Count);
+
+                    await bot.SendMessage(update.Message.Chat.Id, questions[randomInd].Text);
+
+
+                    chatId = (int)update.Message.Chat.Id;
                 }
             }
+            else
+            {                
+                if (update.Message.Text == questions[randomInd].RightAnswer)
+                {
+                    correctAnswersCount++;
+                    await bot.SendMessage(chatId, "Базару нет");
+                    questions.RemoveAt(randomInd);
+                }
+                else
+                {
+                    await bot.SendMessage(chatId, "ну неправильно бр");
+                    questions.RemoveAt(randomInd);
+                }
 
+                randomInd = new Random().Next(0, questions.Count);
+                if (questions.Count == 0)
+                {
+
+                    
+                    await bot.SendMessage(chatId, $"Игра завершена! Ваш диагноз - {SayDiagnosis(correctAnswersCount, questionCount)}");
+                    correctAnswersCount = 0;
+                    return;
+                }
+                await bot.SendMessage(update.Message.Chat.Id, questions[randomInd].Text);
+
+                
+                
+            }
+            static string SayDiagnosis(int cnt, int len)
+            {
+                string[] diagnosises = { "Идиот", "Бездарь", "Дурак", "Человек Разумный", "Талант", "Гений" };
+                double rightAns = cnt;
+                double questionsNumber = len;
+                double percent = rightAns / questionsNumber * 100;
+
+                return diagnosises[(int)(percent / 20d)];
+            }
         }
-        
     }
 }
